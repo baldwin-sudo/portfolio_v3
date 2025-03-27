1943cf8111ac13ad761445c46a058b3d250e43bd
baldwin-sudo <kasmimehdi561@gmail.com>
2025-03-27 03:09:08 +0000

    feat:added the projects data

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import githubIco from "../assets/github-icon.png";
+import linkIco from "../assets//link-icon.png";
+import StackIcon from "tech-stack-icons";
+import { p } from "framer-motion/client";
 
 export default function ProjectCard({ project }) {
   const { title, tech, github, demo, description, img } = project;
@@ -7,12 +10,14 @@ export default function ProjectCard({ project }) {
     <div className="bg-neutral-50 hover:bg-neutral-200 border-1 border-neutral-200  flex gap-3 w-fit  max-w-250 rounded-lg p-8 ">
       <div className="flex flex-col gap-5">
         <h1 className="text-3xl font-semibold">{title}</h1>
-        <div>
+        <div className="flex items-center gap-1">
           <span className="font-bold  text-neutral-600">Made with :</span>{" "}
           {tech.map((t, index) => {
             return (
               <p key={index} className="inline">
-                {t}
+                {<StackIcon name={t} className="size-10 lg:size-15" /> || (
+                  <p className="text-black">{t}</p>
+                )}
               </p>
             );
           })}
@@ -20,26 +25,28 @@ export default function ProjectCard({ project }) {
         <p className="text-lg  text-neutral-500">{description}</p>
         <div className="flex gap-10 mt-5">
           {github && (
-            <button className="flex  items-center justify-center gap-1 border-1 rounded-full px-4 sm:px-6 py-3 bg-white text-lg sm:text-xl  cursor-pointer ">
+            <button className="flex  items-center justify-center gap-1 border-1 rounded-full px-4 sm:px-6 py-3 bg-white hover:opacity-80 text-lg sm:text-xl  cursor-pointer ">
               {" "}
               <img className="w-6 h-6" src={githubIco} alt="" />
               Github
             </button>
           )}
           {demo && (
-            <button className="flex  items-center justify-center gap-1  rounded-full px-4 sm:px-6 py-3 bg-neutral-900 text-white text-lg sm:text-xl cursor-pointer ">
-              <img className="w-6 h-6" src={githubIco} alt="" />
-              demo
+            <button className="flex  items-center justify-center gap-1  rounded-full px-4 sm:px-6 py-3 bg-neutral-900 hover:opacity-80 text-white text-lg sm:text-xl cursor-pointer ">
+              <img className="w-6 h-6" src={linkIco} alt="" />
+              <a href={demo}>Live</a>
             </button>
           )}
         </div>
       </div>{" "}
-      <div>
-        <img
-          src={img}
-          alt="project img"
-          className="hidden lg:block w-100 h-80 border-2 "
-        />
+      <div className="flex items-center">
+        {img != null ? (
+          <img
+            src={img}
+            alt="project img"
+            className="hidden lg:block w-140 h-50 border-2 border-neutral-300 "
+          />
+        ) : null}
       </div>
     </div>
   );
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,30 +1,40 @@
+import luxurious from "../assets/luxurious-p.png";
 const projects = [
   {
     title: "Luxurious Clothing",
-    tech: ["react ", "node", "java"],
-    img: "url",
+    tech: ["reactjs"],
+    img: luxurious,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut possimus mollitia excepturi vitae. Pariatur nesciunt tempora alias, inventore illo quis dolorem ratione nihil doloremque ducimus praesentium aspernatur dolorum aperiam doloribus",
-    demo: "hi",
-    github: "hi",
+      "Luxury clothes shop , developped using React js , and fakeStore Api for the data , deployed using netlify .",
+    demo: "https://eluxury.netlify.app/",
+    github: "https://github.com/baldwin-sudo/eluxury",
   },
   {
-    title: "Luxurious Clothing",
-    tech: ["react ", "node", "java"],
-    img: "url",
+    title: "Custom Deep learning Framework",
+    tech: ["python", "pytorch", "rust"],
+    img: null,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut possimus mollitia excepturi vitae. Pariatur nesciunt tempora alias, inventore illo quis dolorem ratione nihil doloremque ducimus praesentium aspernatur dolorum aperiam doloribus",
-    demo: "hi",
-    github: "hi",
+      "I made my own Deep learning framework ,inspired by the design of Pytorch's.Implemented Firstly using Python ,and Secondly a minimal version using Rust.",
+    demo: null,
+    github: "https://github.com/baldwin-sudo/deep-learning-framework",
   },
   {
-    title: "Luxurious Clothing",
-    tech: ["react ", "node", "java"],
-    img: "url",
+    title: "PassKeeper",
+    tech: ["reactjs", "go", "sqlite3"],
+    img: null,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut possimus mollitia excepturi vitae. Pariatur nesciunt tempora alias, inventore illo quis dolorem ratione nihil doloremque ducimus praesentium aspernatur dolorum aperiam doloribus",
-    demo: "hi",
-    github: null,
+      "My own passwords manager , built using React js , Golang and Sqlite .(on going developement)",
+    demo: null,
+    github: "https://github.com/baldwin-sudo/PassKeeper",
+  },
+  {
+    title: "Rag based Chatbot",
+    tech: ["python", "elastic", "openai", "streamlit"],
+    img: null,
+    description:
+      "An AI-powered chatbot with memory and history, built using LangChain and Streamlit. Just upload your PDFs—Lama will parse them, Elasticsearch will index the content, and OpenAI will deliver smart, context-aware responses.",
+    demo: null,
+    github: "https://github.com/baldwin-sudo/baldwin-templates",
   },
 ];
 export default projects;
