9f1cf7fa4000684c11f1aae2ab034cc00fc3f0df
baldwin-sudo <kasmimehdi561@gmail.com>
2025-03-27 14:14:34 +0000

    fix:navbar focused menu item

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,9 @@ export default function NavBar({ scrollToSection }) {
   const menuItemsRefs = useRef([]);
   const onClick = (e, index) => {
     e.preventDefault();
+
     const menuItem = menuItemsRefs.current[index];
+
     if (!menuItem) return;
 
     const { offsetLeft, offsetTop, offsetWidth, offsetHeight } = menuItem;
@@ -29,9 +31,9 @@ export default function NavBar({ scrollToSection }) {
     });
   };
   return (
-    <div className="z-10 bg-white/60  backdrop-blur-3xl fixed top-0 left-1/2 -translate-x-1/2 sm:m-5 flex flex-wrap  items-center justify-center  sm:rounded-full w-full sm:w-fit  sm:flex-nowrap gap-1 sm:gap-8 py-4 px-15 sm:px-4     shadow-lg">
+    <div className="z-10 bg-white/70  backdrop-blur-3xl fixed top-0 left-1/2 -translate-x-1/2 sm:m-5 flex flex-wrap  items-center justify-center  sm:rounded-full w-full sm:w-fit  sm:flex-nowrap gap-1 sm:gap-8 py-4 px-15 sm:px-4     shadow-lg">
       <motion.div
-        className=" bg-neutral-200 rounded-full "
+        className=" bg-neutral-100 rounded-full  shadow-sm"
         // Animate when this value changes:
         animate={{
           left: bubleDimensions.left,
@@ -57,7 +59,11 @@ export default function NavBar({ scrollToSection }) {
               onClick(e, index);
               setCurrentMenuItem(item);
             }}
-            className="text-neutral-500 hover:text-black  rounded-full px-3 py-1.5"
+            className={`rounded-full px-3 py-1.5 transition-colors ${
+              currentMenuItem === item
+                ? "text-black font-semibold"
+                : "text-neutral-600 hover:text-black"
+            }`}
           >
             {item}
           </button>
