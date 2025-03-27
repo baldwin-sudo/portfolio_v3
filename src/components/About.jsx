6e4c728f7a61a90d22e3f41961dd6eb71a85fbfb
baldwin-sudo <kasmimehdi561@gmail.com>
2025-03-27 13:26:36 +0000

    fix:added github and linkedin profile urls

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,14 +26,20 @@ const About = forwardRef((props, ref) => {
         </p>
       </div>
       <div className="flex gap-2 sm:gap-4 mt-5 sm:mt-15 ">
-        <button className="flex  items-center justify-center gap-3 rounded-full px-5 sm:px-10 py-3 bg-white text-xl sm:text-2xl lg:text-3xl cursor-pointer transition-all duration-200 hover:-translate-y-3">
+        <a
+          href={"https://github.com/baldwin-sudo"}
+          className="flex  items-center justify-center gap-3 rounded-full px-5 sm:px-10 py-3 bg-white text-xl sm:text-2xl lg:text-3xl cursor-pointer transition-all duration-200 hover:-translate-y-1"
+        >
           <img className="w-10 h-10" src={githubIco} alt="" />
           Github
-        </button>
-        <button className="flex  items-center justify-center gap-3 rounded-full px-5 sm:px-10 py-3 bg-neutral-900 border-2 border-white text-white  text-xl  sm:text-2xl lg:text-3xl cursor-pointer transition-all duration-200 hover:-translate-y-3">
+        </a>
+        <a
+          href={"https://www.linkedin.com/in/el-mehdi-kasmi-04ab45231/"}
+          className="flex  items-center justify-center gap-3 rounded-full px-5 sm:px-10 py-3 bg-neutral-900 border-2 border-white text-white  text-xl  sm:text-2xl lg:text-3xl cursor-pointer transition-all duration-200 hover:-translate-y-1"
+        >
           <img className="w-10 h-10" src={linkdedinIco} alt="" />
           Linkedin
-        </button>
+        </a>
       </div>
     </div>
   );
